3582d049da2fceb82fce49c7d268ef90d7ef72a4
Anthony Pruniere <anthony.pruniere@wanadoo.fr>
2025-09-25 10:09:23 +0200

    add fech api meteo

--- src/main.js
+++ src/main.js
@@ -1,11 +1,47 @@
 import './style.css'
 import {apiKey} from './env.js'
 
-const meteo = fetch("https://api.openweathermap.org/data/2.5/weather?lon=1.44&lat=43.6&appid=" 
-+ apiKey).then(response=>{
-  return response
-}).then(data=> {
-  return data.json();
+// FETCH RECUPERER INFOS (JSON) D'UNE API: 
+
+// const meteo = fetch("https://api.openweathermap.org/data/2.5/weather?lon=1.44&lat=43.6&appid=" 
+// + apiKey).then(response=>{
+//   return response
+// }).then(data=> {
+//   return data.json();
+// });
+// console.log(meteo);
+
+//OU:
+
+// const meteo = fetch("https://api.openweathermap.org/data/2.5/weather?lon=1.44&lat=43.6&appid=" 
+// + apiKey).then(response=> response.json())
+// .then(data =>{
+//   console.log(data);
+// });
+
+//OU:
+
+// const apiMeteo = async () => {
+//   return await fetch("https://api.openweathermap.org/data/2.5/weather?lon=1.44&lat=43.6&appid=" + apiKey)
+//   .then(response => {
+//     return response.json();
+//   });
+// };
+// apiMeteo().then(json => {
+//   console.log(json);
+// })
+
+//OU:
+
+async function apiFunction(){
+  return await fetch("https://api.openweathermap.org/data/2.5/weather?lon=1.44&lat=43.6&appid=" + apiKey)
+  .then(response => {
+    return response.json();
+  });
+};
+
+apiFunction().then(json =>{
+  console.log(json.weather[0].main);
 });
 
-console.log(meteo);
+
